93ceb411d7ebc786740f577a04e72b03ccd212a3
Melissa Funk <melissafunk85@gmail.com>
2021-07-31 09:15:32 -0700

    hover fixes

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $( "#hotDogMan" ).mouseover(function(){
-        $(this).attr("src", "IMAGES/talking-hot-dog-man.png");
-    });
-
-    $( "#hotDogMan" ).mouseout(function(){
-        $(this).attr("src", "IMAGES/hot-dog-man.png");
-    });
-});

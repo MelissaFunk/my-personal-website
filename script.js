74fa52e0da842710c8e498743729a984ffa64817
Melissa Funk <melissafunk85@gmail.com>
2021-08-04 15:06:06 -0700

    fixing prompt result language

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function howManyHotDogs() {
     var numDogs = prompt ("How many hot dogs can you eat?", "1");
     if (numDogs !=null) {
         document.getElementById("hotdogs").innerHTML =
-        "Wow, you can eat" + numDogs + "! Cool, dude.";
+        "Wow, you can eat " + numDogs + " ! Cool, dude.";
     }
 }

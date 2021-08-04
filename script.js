c59bc19b7c9984d42a2b41ae77ac20d1e3d3e105
Melissa Funk <melissafunk85@gmail.com>
2021-08-04 15:10:40 -0700

    added if results for # of dogs

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function howManyHotDogs() {
     var numDogs = prompt ("How many hot dogs can you eat?", "1");
-    if (numDogs !=null) {
+    if (numDogs <= 2) {
         document.getElementById("hotdogs").innerHTML =
-        "Wow, you can eat " + numDogs + "! Cool, dude.";
+        "Really, only " + numDogs + "?";
+    }
+    if (numDogs >= 3) {
+        document.getElementById("hotdogs").innerHTML =
+        "Wow, you can eat " + numDogs + "! That's pretty good!";
+    }
+    if (numDogs >= 5) {
+        document.getElementById("hotdogs").innerHTML =
+        "What the hell.. " + numDogs + "? Calm down, dude.";
     }
 }

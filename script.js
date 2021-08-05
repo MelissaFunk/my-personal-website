0db0aa56f98fc3652c13cc4c9cf5b716131dc54a
Melissa Funk <melissafunk85@gmail.com>
2021-08-04 17:19:58 -0700

    function edits

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ function howManyHotDogs() {
     var numDogs = prompt ("How many hot dogs can you eat?", "1");
     if (numDogs <= 2) {
         document.getElementById("hotDogButtonAnswer").innerHTML =
-        "Really, only " + numDogs + "hot dogs?";
+        "Really, only " + numDogs + "?";
     }
     if (numDogs >= 3) {
         document.getElementById("hotDogButtonAnswer").innerHTML =
-        "Wow, you can eat " + numDogs + "hot dogs! That's pretty good!";
+        "Wow, you can eat " + numDogs + " hot dogs! That's pretty good!";
     }
     if (numDogs >= 7) {
         document.getElementById("hotDogButtonAnswer").innerHTML =
-        "Oh " + numDogs + "hot dogs? Brag alert.";
+        "Oh " + numDogs + " hot dogs? Brag alert.";
     }
     if (numDogs >= 20) {
         document.getElementById("hotDogButtonAnswer").innerHTML =
-        "What the hell.. " + numDogs + "hot dogs?? Calm down, dude.";
+        "What the hell.. " + numDogs + " hot dogs?? Calm down, dude.";
     }
 }

e67380237d3ade45322f9be2c334f1e3481cfd0c
Melissa Funk <melissafunk85@gmail.com>
2021-08-04 17:09:34 -0700

    fix button issues

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function howManyHotDogs() {
     var numDogs = prompt ("How many hot dogs can you eat?", "1");
     if (numDogs <= 2) {
-        document.getElementById("hotdogs").innerHTML =
+        document.getElementById("hotDogButtonAnswer").innerHTML =
         "Really, only " + numDogs + "?";
     }
     if (numDogs >= 3) {
-        document.getElementById("hotdogs").innerHTML =
+        document.getElementById("hotDogButtonAnswer").innerHTML =
         "Wow, you can eat " + numDogs + "! That's pretty good!";
     }
     if (numDogs >= 5) {
-        document.getElementById("hotdogs").innerHTML =
+        document.getElementById("hotDogButtonAnswer").innerHTML =
         "What the hell.. " + numDogs + "? Calm down, dude.";
     }
 }

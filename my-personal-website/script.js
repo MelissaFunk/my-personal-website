603cc5b96bc890af6d0abbd371496c0c4fa52899
+ <+>
2021-08-07 12:18:36 -0700

    commit tree 9c50e9a6253ca28a4f94caf22f8d03b8b69bfb63

---
+++ my-personal-website/script.js
@@ -0,0 +1,19 @@
+function howManyHotDogs() {
+    var numDogs = prompt ("How many hot dogs can you eat?", "1");
+    if (numDogs <= 2) {
+        document.getElementById("hotDogButtonAnswer").innerHTML =
+        "Really, only " + numDogs + "?";
+    }
+    if (numDogs >= 3) {
+        document.getElementById("hotDogButtonAnswer").innerHTML =
+        "Wow, you can eat " + numDogs + " hot dogs! That's pretty good!";
+    }
+    if (numDogs >= 7) {
+        document.getElementById("hotDogButtonAnswer").innerHTML =
+        "Oh " + numDogs + " hot dogs? Brag alert.";
+    }
+    if (numDogs >= 20) {
+        document.getElementById("hotDogButtonAnswer").innerHTML =
+        "What the hell.. " + numDogs + " hot dogs?? Calm down, dude.";
+    }
+}
